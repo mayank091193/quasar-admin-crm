b95181682f3444978e7bd6dfb1bf0dc15b327dee
Mayank Patel <mayank091193@gmail.com>
2020-06-18 18:35:00 +0530

    Fixed mobile compatibility issues.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -139,9 +139,9 @@
     <q-page-container>
       <q-page class="row no-wrap">
         <div class="col">
-          <div class="column full-height">
-            <q-scroll-area class="col q-pr-sm" visible>
-              <router-view />
+          <div class="full-height">
+            <q-scroll-area class="col q-pr-sm full-height" visible>
+              <router-view/>
             </q-scroll-area>
           </div>
         </div>
@@ -179,5 +179,9 @@
 
   .tab-active {
     background-color: green;
+  }
+
+  body {
+    background: #f1f1f1 !important;
   }
 </style>
